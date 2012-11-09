4820b9f3f1ab0891dab0008414792f9b321ca0ff
João Netto <hi@joaonetto.me>
2012-11-09 18:28:23 -0200

    Feature: show products by taxonomy filter.

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,5 +1,9 @@
 Spree::Admin::ProductsController.class_eval do
   def stock
-    @products = Spree::Product.active
+    @taxons = Spree::Taxon.where(:parent_id => nil)
+  end
+
+  def stock_show
+    @products = Spree::Product.all(:include => :taxons, :conditions => "spree_taxons.id = #{params[:format]}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Spree::Core::Engine.routes.prepend do
-  namespace :admin do 
-    resources :products do 
+  namespace :admin do
+    resources :products do
     	collection do
     		get :stock
+        get :stock_show
     	end
     end
   end
